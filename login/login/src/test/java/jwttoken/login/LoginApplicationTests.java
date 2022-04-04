cabf804b53f1d0d9b60b9e64ef9d4ab0d1073e0c
jojaeng2 <76645095+jojaeng2@users.noreply.github.com>
2022-04-05 08:54:31 +0900

    Create jwt-token project

---
+++ login/login/src/test/java/jwttoken/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package jwttoken.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
