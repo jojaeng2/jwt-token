78d4350de5a2d469d6f04d901c3c71fa1606fcaf
jojaeng2 <76645095+jojaeng2@users.noreply.github.com>
2022-04-05 15:40:27 +0900

    Jwt Token Example completion

--- login/login/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ login/login/src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -1,13 +1,12 @@
-package jwttoken.login;
+package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginApplication {
+public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginApplication.class, args);
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
-
 }
